4390e5357d3ebd4bbfe8c3037419262186b36d7d
whitebell <nikky.hh@gmail.com>
2017-06-04 00:41:33 +0900

    TabExpandメソッド

--- Whitebell.Library/_Suppression.cs
+++ Whitebell.Library/_Suppression.cs
@@ -3,6 +3,8 @@
 #if DEBUG
 
 [assembly: SuppressMessage("Redundancy", "RCS1163:Unused parameter.", Scope = "member", Target = "~M:Whitebell.Library.Collections.Generic.OrderedDictionary`2.#ctor(System.Runtime.Serialization.SerializationInfo,System.Runtime.Serialization.StreamingContext)")]
+[assembly: SuppressMessage("General", "RCS1079:Throwing of new NotImplementedException.", Scope = "member", Target = "~M:Whitebell.Library.Extension.Extension.TabExpand(System.String,System.Int32,System.Boolean)~System.String")]
+
 [assembly: SuppressMessage("Usage", "RCS1165:Unconstrained type parameter checked for null.", Scope = "Type", Target = "Whitebell.Library.Collections.Generic.OrderedDictionary`2")]
 [assembly: SuppressMessage("Usage", "RCS1165:Unconstrained type parameter checked for null.", Scope = "Type", Target = "Whitebell.Library.Extension.Extension")]
 
