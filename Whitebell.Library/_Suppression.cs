3947434c0415de0ca854e9e812451afca4562b19
whitebell <nikky.hh@gmail.com>
2017-06-11 16:30:53 +0900

    メッセージ抑制

--- Whitebell.Library/_Suppression.cs
+++ Whitebell.Library/_Suppression.cs
@@ -4,7 +4,8 @@
 
 [assembly: SuppressMessage("General", "RCS1079:Throwing of new NotImplementedException.", Scope = "member", Target = "~M:Whitebell.Library.Extension.Extension.TabExpand(System.String,System.Int32,System.Boolean)~System.String")]
 [assembly: SuppressMessage("Redundancy", "RCS1163:Unused parameter.", Scope = "member", Target = "~M:Whitebell.Library.Collections.Generic.OrderedDictionary`2.#ctor(System.Runtime.Serialization.SerializationInfo,System.Runtime.Serialization.StreamingContext)")]
-[assembly: SuppressMessage("Readability", "RCS1191:Declare enum value as combination of names.", Scope = "type", Target = "~T:Whitebell.Library.MapFlag")]
+[assembly: SuppressMessage("Readability", "RCS1191:Declare enum value as combination of names.", Scope = "type", Target = "~T:Whitebell.Library.Kernel32.MapFlag")]
+[assembly: SuppressMessage("Readability", "RCS1191:Declare enum value as combination of names.", Scope = "type", Target = "~T:Whitebell.Library.Extension.StringConvert")]
 
 [assembly: SuppressMessage("Usage", "RCS1165:Unconstrained type parameter checked for null.", Scope = "Type", Target = "Whitebell.Library.Collections.Generic.OrderedDictionary`2")]
 [assembly: SuppressMessage("Usage", "RCS1165:Unconstrained type parameter checked for null.", Scope = "Type", Target = "Whitebell.Library.Extension.Extension")]
