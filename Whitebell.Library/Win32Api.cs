76e940afa76fa47d0030e304c21fc62fe1e55d72
whitebell <nikky.hh@gmail.com>
2018-10-08 12:12:12 +0900

    User32.GetKeyState

--- Whitebell.Library/Win32Api.cs
+++ Whitebell.Library/Win32Api.cs
@@ -430,6 +430,30 @@ namespace Whitebell.Library
         #endregion
     }
 
+    public static class User32
+    {
+        #region GetKeyState (2000 Professional/2000 Server)
+
+        /// <summary>
+        /// Retrieves the status of the specified virtual key.
+        /// The status specifies whether the key is up, down, or toggled (on, off—alternating each time the key is pressed).
+        /// </summary>
+        /// <param name="nVirtKey">
+        /// A virtual key. If the desired virtual key is a letter or digit (A through Z, a through z, or 0 through 9), nVirtKey must be set to the ASCII value of that character.
+        /// For other keys, it must be a virtual-key code.
+        /// If a non-English keyboard layout is used, virtual keys with values in the range ASCII A through Z and 0 through 9 are used to specify most of the character keys.
+        /// For example, for the German keyboard layout, the virtual key of value ASCII O(0x4F) refers to the "o" key, whereas VK_OEM_1 refers to the "o with umlaut" key.</param>
+        /// <returns>
+        /// <para>The return value specifies the status of the specified virtual key, as follows:</para>
+        /// <para>If the high-order bit is 1, the key is down; otherwise, it is up.</para>
+        /// <para>If the low-order bit is 1, the key is toggled.A key, such as the CAPS LOCK key, is toggled if it is turned on.The key is off and untoggled if the low-order bit is 0. A toggle key's indicator light (if any) on the keyboard will be on when the key is toggled, and off when the key is untoggled.</para>
+        /// </returns>
+        [DllImport("User32.dll")]
+        public static extern short GetKeyState(int nVirtKey);
+        //SHORT GetKeyState(int nVirtKey);
+        #endregion
+    }
+
     public static class Winmm
     {
         [CLSCompliant(false)]
